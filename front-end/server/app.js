99b79fffa1adace8459dffdb34dcdea7d44b4167
周瑶 <zhouyao@zkyouxi.com>
2020-07-20 20:04:51 +0800

    项目初始化

---
+++ front-end/server/app.js
@@ -0,0 +1,21 @@
+// app.js
+const Koa = require('koa');
+const app = new Koa();
+const path = require('path');
+const koaStatic = require('koa-static');
+const koaMount = require('koa-mount');
+const favicon = require('koa-favicon');
+
+const isDev = process.env.SSR_ENV === 'dev';
+
+// routes
+const ssr = isDev ? require('./dev.ssr') : require('./prod.ssr');
+
+// Static File Server
+const resolve = file => path.resolve(__dirname, file);
+app.use(favicon(resolve('./favicon.ico')));
+app.use(koaMount('/', koaStatic(resolve('../public'))));
+
+app.use(ssr.routes(), ssr.allowedMethods());
+
+module.exports = app;
